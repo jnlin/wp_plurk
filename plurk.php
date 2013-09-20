608c4a8ad141d4a1b6b012ff5351b66cdc0c3308
Eric Lin <jnlin@Munich.tamama.org>
2013-09-20 14:27:20 +0000

    add settings page

--- plurk.php
+++ plurk.php
@@ -12,18 +12,21 @@ Version: 0.9
 Author URI: http://github.com/jnlin/wp_plurk
 */
 
-require __DIR__ . '/plurkoauth-master/plurkAPI.php';
+require __DIR__ . '/plurkAPI.php';
 
 function do_post_plurk($post_id)
 {
     if (!('publish' == $_POST['post_status'] and 'publish' != $_POST['original_post_status'])) {
-	return;
+//	return;
     }
 
     $post = get_post($post_id);
     $url = get_permalink($post_id);
 
-    $plurk = new PlurkAPI();
+    $plurk = new PlurkAPI(get_option('plurk_consumer_key'),
+	    get_option('plurk_consumer_secret'),
+	    get_option('plurk_token_key'),
+	    get_option('plurk_token_secret'));
 
     $plurk->callAPI('/APP/Timeline/plurkAdd', array(
 		'content' => "$url ({$post->post_title})",
@@ -33,5 +36,76 @@ function do_post_plurk($post_id)
 }
 
 add_action('publish_post', 'do_post_plurk');
+add_action('admin_menu', 'plurk_option_page');
+add_action('admin_init', 'plurk_register_settings' );
 
-?>
+
+function your_plugin_settings_link($links) { 
+    $settings_link = '<a href="options-general.php?page=wp_plurk/plurk.php">' . __('Settings') . '</a>'; 
+    array_unshift($links, $settings_link); 
+    return $links; 
+}
+
+$plugin = plugin_basename(__FILE__); 
+add_filter("plugin_action_links_$plugin", 'your_plugin_settings_link' );
+
+function plurk_register_settings()
+{
+    register_setting('plurk-settings-group', 'plurk_consumer_key');
+    register_setting('plurk-settings-group', 'plurk_consumer_secret');
+    register_setting('plurk-settings-group', 'plurk_token_key');
+    register_setting('plurk-settings-group', 'plurk_token_secret');
+}
+
+function plurk_option_page()
+{
+    add_options_page('Plurk', 'Plurk', 'manage_options', __FILE__, 'plurk_settings_page');
+}
+
+function plurk_settings_page()
+{
+    ?>
+    <div class="wrap">
+    <h2>API Key and Secret</h2>
+
+    <p>Please read <a href="https://github.com/jnlin/wp_plurk/">https://github.com/jnlin/wp_plurk/</a> to get your API key and secret.</p>
+    
+    <form method="post" action="options.php">
+    <?php //wp_nonce_field('update-options'); ?>
+    <!-- starting -->
+    <?php settings_fields('plurk-settings-group'); ?>
+    <?php do_settings_sections('plurk-settings-group'); ?>
+    <!-- ending -->
+    
+    <?php
+    ?>
+
+    <table class="form-table">
+
+    <tr valign="top">
+    <th scope="row">Consumer Key</th>
+    <td><input type="text" name="plurk_consumer_key" value="<?= htmlspecialchars(get_option('plurk_consumer_key')) ?>" style='width:600px;'></td>
+    </tr>
+    <tr valign="top">
+    <th scope="row">Consumer Secret</th>
+    <td><input type="text" name="plurk_consumer_secret" value="<?= htmlspecialchars(get_option('plurk_consumer_secret')) ?>" style='width:600px;'></td>
+    </tr>
+    <tr valign="top">
+    <th scope="row">Token Key</th>
+    <td><input type="text" name="plurk_token_key" value="<?= htmlspecialchars(get_option('plurk_token_key')) ?>" style='width:600px;'></td>
+    </tr>
+    <tr valign="top">
+    <th scope="row">Token Secret</th>
+    <td><input type="text" name="plurk_token_secret" value="<?= htmlspecialchars(get_option('plurk_token_secret')) ?>" style='width:600px;'></td>
+    </tr>
+
+    </table>
+
+    <?php submit_button(); ?>
+
+    </form>
+    
+    </div>
+
+<?php }
+
